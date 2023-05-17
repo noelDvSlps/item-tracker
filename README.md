9a5fabf1118cb71dc514697e7d60c7ac1718774f
Noel Pulido <noelpulido1229@gmail.com>
2023-05-16 20:27:58 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Try this log-in credentials
+
+username: username
+password: password
+
+# To add sample Data
+
+In the project directory, you can run:
+
+### `npm run seed`
+
---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import "./App.css";
+import { User } from "./Pages/User";
+import { AuthProvider } from "./providers/authProvider";
+import { Toaster } from "react-hot-toast";
+
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { RootLayout } from "./layouts/RootLayout";
+import { Main } from "./Pages/main";
+import { UserTools, userToolsLoader } from "./Pages/UserTools";
+import { allItemsLoader } from "./Pages/User";
+import { allUsersLoader } from "./Pages/main";
+import { NotFound } from "./Pages/NotFound";
+import { UserHistory, dataLoader } from "./Pages/UserHistory";
+import { ErrorElement } from "./Pages/ErrorElement";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<Main />} loader={allUsersLoader} />
+
+      <Route
+        // historyLoader={dataLoader}
+        path="User"
+        element={<User />}
+        loader={allItemsLoader}
+        errorElement={<ErrorElement />}
+      />
+
+      <Route
+        path="UserHistory"
+        element={<UserHistory />}
+        loader={dataLoader}
+        errorElement={<ErrorElement />}
+      />
+      <Route
+        path="UserTools"
+        element={<UserTools />}
+        loader={userToolsLoader}
+        errorElement={<ErrorElement />}
+      />
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider>
+        <Toaster />
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
